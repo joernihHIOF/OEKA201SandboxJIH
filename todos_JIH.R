1118576399c2aae91fb9a3c9e68f35a72b166bc7
joernihHIOF <jorn.i.halvorsen@hiof.no>
2025-01-21 13:19:16 +0100

    sttwo

--- todos_JIH.R
+++ todos_JIH.R
@@ -5,10 +5,10 @@ sum(numbers)
 ## Task 2
 # help(sqrt)
 ## Task 3
-rnorm(n=100, mean = 0, sd = 1)
+rnorm(n=100, mean = 0, sd = 2)
 ## Task 4
 now <- as.numeric(substring(paste(Sys.Date()),1,4))
-started <- 2020
-born <- 1995
+started <- 2022
+born <- 2005
 spent <- (now-started)/(now-born)*100
 round(spent, digits=4)
