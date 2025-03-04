ac1ffc6cba95b28a8854e4a1a11c6cd26136633c
joernih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export EMAIL=jorn.halvorsen@gmail.comexport EDITOR=/usr/bin/nvimexport BROWSER=/usr/bin/braveexport LANG=en_US.UTF-8export GIT_PAT=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alias updABC='source /home/joernih/InitABC/init.sh; updmac' alias updJIH='source /home/joernih/InitJIH/init.sh; updmac' alias updBAS='source /home/joernih/InitBAS/init.sh; updmac' # git remote set-url origin https://@github.com/joernih/InitJIH.git# git remote set-url origin https://@github.com/joernih/Hyperinflation <jorn.halvorsen@gmail.com>
2025-03-04 14:32:47 +0100

    latest

--- lectures/lect_datawrang.R
+++ lectures/lect_datawrang.R
@@ -5,18 +5,21 @@ library(datasets)
 library(dplyr)
 library(writexl)
 library(googlesheets4)
+library(ggplot2)
 install_github("joernih/OEKA201Assignment")
 install.packages("googlesheets4")
-|#install_github("joernih/https://github.com/joernih/WASMA")
-
-
+df_iris  <- iris
+nb_ts <- get(load("../data/nb_ts.rda"))
+fred_ts <- get(load("../data/fred_ts.rda"))
 
 ### I. Importing data to R ###
-data(package = "datasets")
-data(package = "OEKA201Assignment")
-## a) From CSV-format
-df_iris  <- iris
-## c) From CSV-format
+## a) Directoy from R 
+df_res <- data(package = "datasets")$results
+df_res
+df_asg <- data(package = "OEKA201Assignment")$results
+df_asg
+
+## b) From CSV-format
 df_wine <- read_csv("https://raw.githubusercontent.com/joernih/OEKA201Assignment/refs/heads/main/data-raw/csv/wine.csv")
 ## c) From Spreadsheets
 ### ca) Excel
@@ -30,12 +33,12 @@ summary(df_gosp)
 ### II. Data transformation in R and Python ###
 # The dplyr package
 ## Application 1
-df_app0 <- data.frame(
+df_app1 <- data.frame(
   id = c(1, 2, 3, 4, 5),
   name = c("Alice", "Bob", "Charlie", "David", "Eve"),
   age = c(25, 30, 35, 40, 45)
 )
-df_app0out <- df_app0 %>%
+app1out <- df_app1 %>%
   # Insert comment
   filter(age > 30) %>%
   # Insert comment
@@ -52,23 +55,132 @@ df_app0out <- df_app0 %>%
 ## Application 2
 df_app2out <- df_wine %>%
   # Insert comment
-	na.omit()
+   na.omit()
+df_app2out
 
-#nb_ts
-#fred_ts[[2]]
-#fred_ts[[2]]
-#data_vk <- data.frame(nb_ts[[1]][[1]])
 
 ## Application 3
-df_app3out <- df_app0
+### Settings
+startd_i <- '2016-01-01'
+startd_n <- '2021-10-05'
+vexr <- c("value_EUR","value_USD")[1]
+
+app3out <- data.frame(nb_ts[[1]][[1]]) %>% 
+ dplyr::mutate(date=base::as.Date(dato)) %>%
+  # Insert comment
+ dplyr::filter(date>startd_i) %>%
+  # Insert comment
+ dplyr::filter(date<startd_n) 
+
+ggplot2::ggplot(app3out , aes(x = date, y =!!as.name(vexr))) + 
+  # Insert comment
+  ggplot2::geom_point() +
+  # Insert comment
+  ggplot2::labs(x = "Date", y = "Exchange Rate", color = "Currency") +
+  ggplot2::theme_classic()
+
 ## Application 4a
-df_app4aout <- df_app0 #%>%
+### Settings
+mvar <- c("mb3","cpi","une","mba","int","gdp")[1]
+vd1 <- as.Date("2022-01-01")
+vd2 <- as.Date("2020-02-01")
+startd_i <- '2020-01-01'
+startd_n <- '2024-01-01'
+plv <- c('nvalue','growth')[1]
+
+### Data transformation
+app4aout <- 
+ fred_ts[[2]] %>%
+  # Insert comment
+ dplyr::filter(id==mvar,country=="NOR") %>%
+  # Insert comment
+ dplyr::mutate(date=base::as.Date(date)) %>%
+  # Insert comment
+ dplyr::filter(date>startd_i) %>%
+  # Insert comment
+ dplyr::filter(date<startd_n) %>%
+  # Insert comment
+ dplyr::mutate(nvalue=100*value/value[1]) %>%
+  # Insert comment
+ dplyr::mutate(lnalue=log(value)) %>% 
+  # Insert comment
+ dplyr::mutate(lvalue=dplyr::lag(value,n=12)) %>% 
+  # Insert comment
+ dplyr::mutate(growth=round(value/lvalue-1, 6)*100) 
+
+# Finding the mean values for all variables in fred_ts
+mv1 <- mean(app4aout[[plv]], na.rm=TRUE)
+
+### Plot
+summary(app4aout,5)
+
+### Plot
+pp1 <- ggplot(app4aout,aes(x = date, y = !!as.name(plv))) + 
+  geom_point() + # ...
+  geom_vline(xintercept = as.numeric(vd1), linetype = "dashed", color = "blue") + # ...
+  geom_vline(xintercept = as.numeric(vd2), linetype = "dashed", color = "blue") + # ...
+  geom_label(y=100,x=as.numeric(vd1),label="Pandemi sluttfase") + # ...
+  geom_label(y=100,x=as.numeric(vd2),label="Pandemi startfase") + # ...
+  geom_hline(yintercept = mv1, linetype = "dashed", color = "red") + # ...
+  geom_line() + 
+  theme_classic()
+pp1
+
 ## Application 4b
-df_app4bout <- df_app0 #%>%
+### Settings
+lnd <- c("USA","JPN","EUZ","GBR","CAN","NOR","DEN","SWE","SKE")[c(6,1,2,3)]
+vd1 <- as.Date("2022-01-01")
+vd2 <- as.Date("2020-02-01")
+startd_i <- '2020-01-01'
+startd_n <- '2024-01-01'
+mvar <- c("mb3","cpi","une","mba","int","gdp")[1]
+plv <- c('nvalue','growth')[1]
+
+### Data transformation
+app4bout <- 
+ fred_ts[[2]] %>%
+ # Insert comment
+ dplyr::filter(id==mvar,country%in%lnd) %>%
+ # Insert comment
+ dplyr::mutate(date=base::as.Date(date)) %>%
+ # Insert comment
+ dplyr::filter(date>startd_i) %>%
+ # Insert comment
+ dplyr::filter(date<startd_n) %>%
+ # Insert comment
+ dplyr::group_by(country) %>%
+ # Insert comment
+ dplyr::mutate(couid=paste0(country,id)) %>%
+ # Insert comment
+ dplyr::mutate(nvalue=100*value/value[1]) %>%
+ # Insert comment
+ dplyr::mutate(lnalue=log(value)) %>% 
+ # Insert comment
+ dplyr::mutate(lvalue=dplyr::lag(value,n=12)) %>% 
+ # Insert comment
+ dplyr::mutate(growth=round(value/lvalue-1, 6)*100) %>%
+ dplyr::ungroup() 
+mv2 <- mean(app4bout[[plv]], na.rm=TRUE)
+
+### Summary
+summary(app4bout,5)
+
+### Plot
+ggplot2::ggplot(data=app4bout, aes(x = date, y =!!as.name(plv))) +
+  geom_line(aes(color = couid)) + ggplot2::labs(title='Tidsserier') + 
+  geom_label(y=100,x=as.numeric(vd1),label="Pandemi sluttfase") + 
+  geom_label(y=100,x=as.numeric(vd2),label="Pandemi startfase") +
+  ggplot2::theme_minimal()
+
 
 ### III. Exporting data from R and Python ###
-## To R data format (hyperfast!)
-## To CSV
+## a) To R data format (hyperfast!)
+library(usethis)
+# You can yourself determine the file path
+save(df_app2out, file = "data/abc.rda")
+
+
+## b) To CSV
 data <- data.frame(x = c(1, 2, 3), y = c("A", "B", "C"))
 write.csv(data, "output.csv", row.names = FALSE)
 ## To spreadsheets
@@ -80,8 +192,8 @@ data <- data.frame(player=c('A', 'B', 'C', 'D'),
 				runs=c(100, 200, 408, NA),
 				wickets=c(17, 20, NA, 5))
 
-# Import data in dataframe to an excel sheet
+# c) Import data in dataframe to an excel sheet
 ## Excel
-write_xlsx(data, "write.xlsx")
+write_xlsx(df_iris, "write.xlsx")
 ## Google sheet
 
