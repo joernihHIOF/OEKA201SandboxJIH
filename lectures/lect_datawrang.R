4c907be1d0d9ccb17513e0b64af33cdfbda51cc6
joernih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export EMAIL=jorn.halvorsen@gmail.comexport EDITOR=/usr/bin/nvimexport BROWSER=/usr/bin/braveexport LANG=en_US.UTF-8export GIT_PAT=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alias updABC='source /home/joernih/InitABC/init.sh; updmac' alias updJIH='source /home/joernih/InitJIH/init.sh; updmac' alias updBAS='source /home/joernih/InitBAS/init.sh; updmac' # git remote set-url origin https://@github.com/joernih/InitJIH.git# git remote set-url origin https://@github.com/joernih/Hyperinflation <jorn.halvorsen@gmail.com>
2025-03-04 11:52:30 +0100

    upd

--- lectures/lect_datawrang.R
+++ lectures/lect_datawrang.R
@@ -1,19 +1,70 @@
 # Installing packages
-
-# Importing data to R and Python
-## From R and Python packages
-ls(packages:datasets)
-#data()
-iris <- datasets::iris
-head(iris)
-2+2
-
+install_github("joernih/OEKA201Assignment")
+library(readr)
+library(datasets)
+library(devtools)
+library(dplyr)
+library(writexl) 
+data(package = "datasets")
+data(package = "OEKA201Assignment")
+# Importing data to R 
+dfrimp  <- iris
 ## From CSV-format
+dfcsvimp <- read_csv("https://raw.githubusercontent.com/joernih/OEKA201Assignment/refs/heads/main/data-raw/csv/wine.csv")
 ## From Spreadsheets
+### Excel
+data_read_exc <- readxl::read_excel("lect1_exc.xlsx")
+#summary(data_read_exc)
+
+### Google
+
 # Data transformation in R and Python
 # The dplyr package
+## Application 0
+df_app0 <- data.frame(
+  id = c(1, 2, 3, 4, 5),
+  name = c("Alice", "Bob", "Charlie", "David", "Eve"),
+  age = c(25, 30, 35, 40, 45)
+)
+df_app0out <- df_app0 %>%
+  # Insert comment
+  filter(age > 30) %>%
+  # Insert comment
+  select(id, name) %>%
+  # Insert comment
+  mutate(name = paste("Hello", name)) %>%
+  # Insert comment
+  arrange(desc(id)) %>%
+  # Insert comment
+  rename(user_id = id) %>%
+  # Insert comment
+  slice(1:3) 
+## Application 2
+df_app2out <- df_app0 #%>%
+## Application 3
+df_app3out <- df_app0 #%>%
+## Application 4a
+df_app4aout <- df_app0 #%>%
+## Application 4b
+df_app4bout <- df_app0 #%>%
+
 # Exporting data from R and Python
+
 ## To R data format (hyperfast!)
 ## To CSV
+data <- data.frame(x = c(1, 2, 3), y = c("A", "B", "C"))
+write.csv(data, "output.csv", row.names = FALSE)
 ## To spreadsheets
+# import writexl library 
+library(writexl) 
+
+# Create a data frame 
+data <- data.frame(player=c('A', 'B', 'C', 'D'), 
+				runs=c(100, 200, 408, NA), 
+				wickets=c(17, 20, NA, 5)) 
+
+# Import data in dataframe to an excel sheet 
+## Excel
+write_xlsx(data, "write.xlsx")
+## Google sheet
 
