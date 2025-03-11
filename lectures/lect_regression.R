b38de0a03e8b5d75e271f564c305435d4fc89692
jorn.halvorsen <jorn.i.halvorsen@hiof.no>
2025-03-11 15:42:57 +0100

    abc

---
+++ lectures/lect_regression.R
@@ -0,0 +1,64 @@
+# Libraries
+library(readr)
+library(broom)
+library(gretlR)
+suppressPackageStartupMessages(library(dplyr))
+# Reading data
+
+##¤ 1. model specifications ###
+#### Model 1
+asbjmlv1 <- "price_i=\\beta_0+\\beta_1 Age_i"
+asbjmev1 <- price ~ Age 
+
+#### Model 2
+asbjmlv2 <- "price_i = \\beta_0+\\beta_1 Age_i + \\beta_2 WinterRain_i + \\beta_3 temp_i + \\beta_4 HarvestRain_i"
+asbjmev2 <- price ~ Age + WinterRain + temp + HarvestRain 
+
+#### Model 3
+asbjmlv3 <- "price_i = \\beta_0+\\beta_1 Age_i + \\beta_2 WinterRain_i + \\beta_3 temp_i + \\beta_4 HarvestRain_i + \\beta_5 Dheavyraint"
+asbjmev3 <- price ~ Age + WinterRain + temp + HarvestRain + Dheavyraint
+
+### 2. Data import and transformations ###
+iwine <- readr::read_csv("https://raw.githubusercontent.com/joernih/OEKA201Assignment/refs/heads/main/data-raw/csv/wine.csv",show_col_types = FALSE)
+names(iwine)
+# [1] "obs"         "price"       "WinterRain"  "temp"        "HarvestRain" "Age"        
+# [1] "obs"         "price"       "WinterRain"  "temp"        "HarvestRain" "Age"        
+
+# Generate a new variable Dheavyraint that is equal to one for the vintages in the sample with very high levels of rainfall in the harvest season (more than 200 mm) and 0 otherwise.
+hlim <- 200
+owine <- iwine %>%
+        # variables in use
+        dplyr::select(price, WinterRain, temp, HarvestRain, Age) %>%
+        ## interaction effects
+        dplyr::mutate(Dheavyraint=ifelse(HarvestRain>hlim,1,0)) %>%
+        ## na ommit
+        stats::na.omit()
+
+#View(owine)
+
+### Graphical
+#pgr <- pairs(owine)
+
+### Numerical
+sds <- summary(owine)
+cds <- cor(owine)
+
+### 4. Model estimations ###
+mod1 <- lm(asbjmev1,data=owine)
+mod2 <- lm(asbjmev2,data=owine)
+mod3 <- lm(asbjmev3,data=owine)
+mot1 <- broom::tidy(mod1)
+mot2 <- broom::tidy(mod2)
+mot3 <- broom::tidy(mod3)
+moa1 <- broom::augment(mod1)
+moa2 <- broom::augment(mod2)
+moa3 <- broom::augment(mod3)
+mog1 <- broom::glance(mod1)
+mog2 <- broom::glance(mod2)
+mog3 <- broom::glance(mod3)
+
+### 5. Results ###
+## Estimation
+summary(mod1)
+summary(mod2)
+summary(mod3)
