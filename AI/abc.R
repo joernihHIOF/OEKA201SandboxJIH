8bdbd88e5275aef2c46fb1d1a2aadc7412b796c1
joernih <jorn.halvorsen@gmail.com>
2026-01-13 15:17:33 +0100

    program

--- AI/abc.R
+++ AI/abc.R
@@ -1,9 +1,16 @@
+library(ellmer)
+library(chatgpt)
+pq <- "2 kjente norske programmere"
+
 # Example with library ellmer
+lmod <- c("openai/gpt-4.1",
+          "groq/llama3-8b-8192",
+          "google_gemini/gemini-2.5-flash",
+          "anthropic/claude-sonnet-4-20250514",
+          "perplexity/sonar")[1]
 
-
-
+objc <- chat(lmod)
+objc$chat(pq)
 # Example with library chatgpt
-
-
-
+ask_chatgpt(pq)
 
