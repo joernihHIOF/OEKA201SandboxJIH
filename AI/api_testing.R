76ae20c6d421802c4e2675a751b0beae5c2c195f
joernih <jorn.halvorsen@gmail.com>
2026-01-21 13:36:01 +0100

    withoutelephant

--- AI/api_testing.R
+++ AI/api_testing.R
@@ -2,7 +2,7 @@
 library(ellmer)
 pq <- "2 kjente norske programmere"
 # OpenAI
-objc <- chat("openai/gpt-4.1")
+objc <- chat("openai/gpt-5.1")
 objc$chat(pq)
 # Google Gemini
 objg <- chat("google_gemini/gemini-2.5-flash")
@@ -10,6 +10,8 @@ objg$chat(pq)
 # Anthropic (Claude)
 obja <- chat("anthropic/claude-3-5-sonnet")
 obja$chat(pq)
+
+
 # ChatGPT
 library(chatgpt)
 pq <- "2 kjente norske programmere"
