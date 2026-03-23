d62af5e1ffb58a81aa0a0c2f2d46ad66629801a4
joernih <jorn.halvorsen@gmail.com>
2026-03-23 11:14:47 +0100

    forsjekk

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # OEKA201SandboxJIH
 
+
+
--- dplyr/data_manip.R
+++ dplyr/data_manip.R
@@ -5,7 +5,7 @@ wine <- readr::read_csv("https://raw.githubusercontent.com/joernih/OEKA201Assign
 iwine <- readr::read_csv("https://raw.githubusercontent.com/joernih/OEKA201Assignment/refs/heads/main/data-raw/csv/wine.csv",show_col_types = FALSE)
 iadvt <- readr::read_csv("https://raw.githubusercontent.com/joernih/OEKA201Assignment/refs/heads/main/data-raw/csv/adverticing.csv",show_col_types = FALSE)
 itssl <- readr::read_csv("https://raw.githubusercontent.com/joernih/OEKA201Assignment/refs/heads/main/data-raw/csv/tssales.csv",show_col_types = FALSE)
-mvardee <- read.csv("https://raw.githubusercontent.com/joernih/WASMP/refs/heads/main/data-raw/csv/mvardf.csv", comment.char="#") 
+madfv <- read.csv("https://raw.githubusercontent.com/joernih/WASMP/refs/heads/main/data-raw/csv/mvardf.csv", comment.char="#") 
 
 #### Application 1: Chatgpt generated
 df_app0 <- data.frame(
@@ -13,17 +13,17 @@ df_app0 <- data.frame(
   name = c("Alice", "Bob", "Charlie", "David", "Eve"),
   age = c(25, 30, 35, 40, 45)
 )
-app1out <- df_app0 %>%
+app1out <- df_app0 |>
   # Insert comment
-  filter(age > 30) %>%
+  filter(age > 30) |>
   # Insert comment
-  select(id, name) %>%
+  select(id, name) |>
   # Insert comment
-  mutate(name = paste("Hello", name)) %>%
+  mutate(name = paste("Hello", name)) |>
   # Insert comment
-  arrange(desc(id)) %>%
+  arrange(desc(id)) |>
   # Insert comment
-  rename(user_id = id) %>%
+  rename(user_id = id) |>
   # Insert comment
   slice(1:3)
 print(app1out)
@@ -42,7 +42,7 @@ ggplot2::ggplot(data=app2out,aes(x = temp, y = price)) + geom_line()
 #
 #
 ##### Application 3a: {{< var app.nrc >}}
-### R
+# Settings
 mvar <- c("mb3","cpi","une","mba","int","gdp")[1]
 vd1 <- as.Date("2022-01-01")
 vd2 <- as.Date("2020-02-01")
@@ -51,31 +51,31 @@ startd_n <- '2024-01-01'
 plv <- c('nvalue','growth')[1]
 #
 ## Data transformation
-app4aout <- mvardee %>%
+app3aout <- madfv |>
   # Filter data on mb3 (money supply) and NOR (Norway)
-  dplyr::filter(id==mvar,country=="NOR") %>%
+  dplyr::filter(id==mvar,country=="NOR") |>
   # Change data type for variable date to Date
-  dplyr::mutate(date=base::as.Date(date)) %>%
+  dplyr::mutate(date=base::as.Date(date)) |>
   # Filter out all observations starting earlier than startd_i
-  dplyr::filter(date>startd_i) %>%
+  dplyr::filter(date>startd_i) |>
   # Filter out all observations starting later or equal than startd_n
-  dplyr::filter(date<startd_n) %>%
+  dplyr::filter(date<startd_n) |>
   # Creating variable nvalue to normalize the dataset
-  dplyr::mutate(nvalue=100*value/value[1]) %>%
+  dplyr::mutate(nvalue=100*value/value[1]) |>
   # Creating variable lnvalue as the natural logarithm
-  dplyr::mutate(lnalue=log(value)) %>%
+  dplyr::mutate(lnalue=log(value)) |>
   # Creating lvalue as the lagged variable 12 months back in time
-  dplyr::mutate(lvalue=dplyr::lag(value,n=12)) %>%
+  dplyr::mutate(lvalue=dplyr::lag(value,n=12)) |>
   # Creating variable for year growth
   dplyr::mutate(growth=round(value/lvalue-1, 6)*100)
   # Finding the mean values for all variables in fred_ts
   #dplyr::mutate(m
-mv1 <- mean(app4aout[[plv]], na.rm=TRUE)
+mv1 <- mean(app3aout[[plv]], na.rm=TRUE)
 #**Table**
-summary(app4aout,5)
+summary(app3aout,5)
 
 #**Plot**
-pp1 <- ggplot(app4aout,aes(x = date, y = !!as.name(plv))) +
+pp1 <- ggplot(app3aout,aes(x = date, y = !!as.name(plv))) +
   geom_point() + # ...
   geom_vline(xintercept = as.numeric(vd1), linetype = "dashed", color = "blue") + # ...
   geom_vline(xintercept = as.numeric(vd2), linetype = "dashed", color = "blue") + # ...
@@ -97,35 +97,35 @@ startd_n <- '2024-01-01'
 mvar <- c("mb3","cpi","une","mba","int","gdp")[1]
 plv <- c('nvalue','growth')[1]
 
-app4bout <- mvardee |>
+app3bout <- app3aout |>
  # Filter data on mb3 (money supply)
- dplyr::filter(id==mvar,country%in%lnd) %>%
+ dplyr::filter(id==mvar,country%in%lnd) |>
   # Change data type for variable date to Date
- dplyr::mutate(date=base::as.Date(date)) %>%
+ dplyr::mutate(date=base::as.Date(date)) |>
  # Filter out all observations starting earlier or equal than startd_i
- dplyr::filter(date>startd_i) %>%
+ dplyr::filter(date>startd_i) |>
  # Filter out all observations starting later or equal than startd_n
- dplyr::filter(date<startd_n) %>%
+ dplyr::filter(date<startd_n) |>
  # Group for each country included in lnd
- dplyr::group_by(country) %>%
+ dplyr::group_by(country) |>
  # Create id-variable for each country
- dplyr::mutate(couid=paste0(country,id)) %>%
+ dplyr::mutate(couid=paste0(country,id)) |>
  # Creating variable lnvalue as the natural logarithm for each country
- dplyr::mutate(nvalue=100*value/value[1]) %>%
+ dplyr::mutate(nvalue=100*value/value[1]) |>
  # Creating variable lnvalue as the natural logarithm
- dplyr::mutate(lnalue=log(value)) %>%
+ dplyr::mutate(lnalue=log(value)) |>
  # Creating lvalue as the lagged variable 12 months back in timefor each country
- dplyr::mutate(lvalue=dplyr::lag(value,n=12)) %>%
+ dplyr::mutate(lvalue=dplyr::lag(value,n=12)) |>
  # Creating variable for year growth
- dplyr::mutate(growth=round(value/lvalue-1, 6)*100) %>%
+ dplyr::mutate(growth=round(value/lvalue-1, 6)*100) |>
  dplyr::ungroup()
-mv2 <- mean(app4bout[[plv]], na.rm=TRUE)
+mv2 <- mean(app3bout[[plv]], na.rm=TRUE)
 #
 #**Table**
-summary(app4bout,5)
+summary(app3bout,5)
 
 ## **Plot**
-ggplot2::ggplot(data=app4bout, aes(x = date, y =!!as.name(plv))) +
+ggplot2::ggplot(data=app3bout, aes(x = date, y =!!as.name(plv))) +
   geom_line(aes(color = couid)) + ggplot2::labs(title='Tidsserier') +
   geom_label(y=100,x=as.numeric(vd1),label="Pandemi sluttfase") +
   geom_label(y=100,x=as.numeric(vd2),label="Pandemi startfase") +
